33c13af08c69ae81f43e05d10ee634ff5eeb19bc
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-28 17:24:13 +0530

    Heap sort and Quick sort

---
+++ Sorting_algorithms/Heap_sort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_and_searching_techniques.Sorting_algorithms
+{
+    internal class Heap_sort
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the elements:");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nOriginal Array:");
+            PrintArray(arr);
+
+            HeapSort(arr);
+
+            Console.WriteLine("\nSorted Array:");
+            PrintArray(arr);
+
+            Console.ReadLine();
+        }
+        public static void HeapSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // Build max heap
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+
+            // Extract elements one by one from the heap
+            for (int i = n - 1; i > 0; i--)
+            {
+                Swap(arr, 0, i);
+                Heapify(arr, i, 0);
+            }
+        }
+        private static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest])
+                largest = left;
+
+            if (right < n && arr[right] > arr[largest])
+                largest = right;
+
+            if (largest != i)
+            {
+                Swap(arr, i, largest);
+                Heapify(arr, n, largest);
+            }
+        }
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach (int value in arr)
+                Console.Write(value + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sorting_algorithms/Quick_sort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Sorting_and_searching_techniques.Sorting_algorithms
+{
+    internal class Quick_sort
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 9, 3, 7, 5, 6, 4, 8, 2, 1 };
+
+            Console.WriteLine("Original Array:");
+            PrintArray(arr);
+
+            QuickSort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine("\nSorted Array:");
+            PrintArray(arr);
+
+            Console.ReadLine();
+        }
+
+        
+        public static void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int partitionIndex = Partition(arr, low, high);
+
+                QuickSort(arr, low, partitionIndex - 1);
+                QuickSort(arr, partitionIndex + 1, high);
+            }
+        }
+
+        
+        private static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, high);
+            return i + 1;
+        }
+
+        
+        private static void Swap(int[] arr, int i, int j)
+        {
+            
+            (arr[i], arr[j]) = (arr[j], arr[i]);
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach (int value in arr)
+                Console.Write(value + " ");
+            Console.WriteLine();
+        }
+    }
+}
