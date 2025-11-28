c207637ec23c75a36905745d5b2afa642b1cf146
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-28 14:57:13 +0530

    Insertion sort

---
+++ Sorting_algorithms/Insertion_sort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_and_searching_techniques.Sorting_algorithms
+{
+    internal class Insertion_sort
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements in the array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 1; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+            Console.WriteLine("The sorted array is:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+}
